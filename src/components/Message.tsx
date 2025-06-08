47f282eb571dd14537189a11b32e41e3bef16a26
Sumit Sahani <120613334+sumitmsahani@users.noreply.github.com>
2025-06-09 00:24:17 +0530

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,7 +14,7 @@ const Message = () => {
           Dear Madam Ji,
         </h1>
         <p className="text-xl text-gray-700 leading-relaxed mb-6">
-          Happy Anniversary to the two beautiful souls who brought you into this world — the same souls I silently thank every single day.
+          Happy Anniversary to the two beautiful souls who brought you into this world — the same souls I silently thank every single day.<br></br>
 You know what? I'm not just lucky to have you… I’m incredibly blessed to have become a small part of a family that raised someone as pure, kind, and full of love as you.
 Your mom and dad’s bond is not just a milestone — it’s a reflection of the kind of love I dream of having with you someday.
 Through you, I feel the warmth of your whole family… your mom’s kindness, your dad’s strength, your brother’s energy — it’s like I’ve found a second home in you.
